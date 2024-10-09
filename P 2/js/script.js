a1dac516edc37205de5815b2ab888be10d270232
VORA-JENISH <jenishvora44000@gmail.com>
2024-10-09 14:25:03 +0530

    Add files via upload

---
+++ P 2/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#testimonial_slider").owlCarousel({
+        items:2,
+        margin:10,
+        // loop:true,
+        autoplay:true,
+        autoplaytimeout:5000
+    });
+});
